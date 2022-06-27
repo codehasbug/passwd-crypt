d2df7402193fc869a26011325f0a9bf14f22e873
codehasbug <1960228744@qq.com>
2022-06-27 12:21:04 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # passwd-crypt
 A tool for generate encrypted Linux password.
+
+# Guideline
+## Build
+```shell
+gcc passwd-crypt.c -lcrypt -o passwd-crypt 
+```
+## Install
+```shell
+mv passwd-crypt /usr/bin
+```
+## Usage
+The tool requests two arguments, a password which you want to encrypt and a salt.  
+Usage: passwd-crypt [password without encrypt] [input 0 generate a random salt or input a specific salt]  
+Example: 
+```shell
+passwd-crypt YOUR_PASSWORD 0
+```
+
---
+++ passwd-crypt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <crypt.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("Usage: %s [password without encrypt] [input 0 generate a random salt or input a specific salt]\n", argv[0]);
+    return -1;
+  }
+
+  if(!strcmp(argv[2], "0")) {
+    argv[2] = crypt_gensalt("SHA512", 0, NULL, 0);
+  }
+
+  char *buf = crypt((const char *)argv[1], (const char *)argv[2]);
+  if(buf == NULL) {
+    printf("Encrypt error!\n");
+    return -1;
+  }
+  
+  printf("crypt: %s\n", buf);
+  return 0;
+}
